94f0d844a0fbe7935bc40c7c796a9cc378844946
Nikolay <discovernick@gmail.com>
2023-02-21 11:23:12 +1100

    Further Updates: Basics Complete

--- SysPrintFunc/src/Main.java
+++ SysPrintFunc/src/Main.java
@@ -30,5 +30,63 @@ public class Main {
         short shortVal = 2000;
         int intVal = 500000;
         long longVal = 50000 + intVal + shortVal + byteVal;
+
+        // double is the default for floating point number - more precise as its larger
+        // Can also use suffixes of f/F or d/D for type declaration
+        int myIntValue = 5;
+        float myFloatValue = 5.0f;
+        double myDoubleValue = 5.0;
+
+        // Divison
+        System.out.println(5 / 2); // Returns 2 - Truncates any fractional value
+        System.out.println(5f / 2f); // Returns 2.5
+        System.out.println(5d / 2d ); // Returns 2.5
+        System.out.println(5f / 3f); // Returns 1.6666666 (More decimal places in memory)
+        System.out.println(5d / 3d ); // Returns 1.6666666666666667 (More decimal places in memory)
+        System.out.println(5.00 / 3 ); // Returns Double
+        //System.out.println(5.00 / 3f ); // Returns Error
+
+        // Convert Pounds to Kg
+        System.out.println(70d * 0.45359237d );
+
+        // When precise calculations are necessary - we can use the Big Decimal type
+
+        // Chars and Strings
+        char singleChar = 'y'; // Have to use single quotes for char, String uses double quotes
+        // Can pull in the UNICODE value
+        // char unicodePull = '⧵u0061'; Does work on JDK 17
+        char myDecimalCode = 68; // Can assign integer value too
+        System.out.println(myDecimalCode);
+
+        // Challenge
+        char mySimpleChar = '?';
+        char myUnicodeChar = "?".charAt(0);
+        char myDecimalChar = 63;
+        System.out.println(mySimpleChar + " & " + myUnicodeChar + " & " + myDecimalChar);
+
+        // Defining Boolean Values
+        boolean trueOrNot = false;
+
+        // String is a special class
+        String myString = "This is a simple String!";
+        String specialChar = "Sum of ⧵u0061 is 6"; // In newer versions of Java this works.
+        System.out.println(specialChar);
+
+        // Concatenation
+        // + operator, if string is concat, and numbers are addition. String plus anything else is a string
+        String someString = "10";
+        someString = 90 + someString;
+        System.out.println(someString);
+
+        // With chars things get different
+        char firstChar = 'a';
+        char secondChar = 'b';
+        System.out.println(firstChar + secondChar); // Returns 195 - sum of ASCII code values
+        System.out.println("" + firstChar + secondChar); // Returns ab due to the first value being a string
+
+        // Reassigning strings multiple times can be super inefficient, so we can use StringBuilder
+        // StringBuilder is mutable but doesn't have the same power
+
+
     }
 }
