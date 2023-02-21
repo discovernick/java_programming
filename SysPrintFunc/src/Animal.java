e3582ff85ef0fae84f82f72c3c7d39fb01904a17
Nikolay <discovernick@gmail.com>
2023-02-22 09:30:22 +1100

    Inheritance Exercises Complete

---
+++ SysPrintFunc/src/Animal.java
@@ -0,0 +1,36 @@
+public class Animal {
+    protected String type;
+    private String size;
+    private double weight;
+
+    // Constructor
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+    // No args constructor
+    public Animal() {
+
+    }
+
+    // To String Method
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public void move(String speed) {
+        System.out.println(type + " moves " + speed);
+    }
+
+    public void makeNoise() {
+        System.out.println(type + " make some kind of noise");
+    }
+}
+
+
